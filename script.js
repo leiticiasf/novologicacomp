757f952ea1dc0684bfc76a6223b1d1705e98d233
Letícia <111643803+leiticiasf@users.noreply.github.com>
2023-08-10 11:51:24 -0300

    maisum

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function voltar(){
 function func(){  
     var saldo = 1000 
     if (document.getElementById ("saldo").checked){
-       document.getElementById("saida").innerHTML = saldo
+       document.getElementById("saida").innerHTML = "Saldo atual R$ " + saldo + ",00"
     }
     else if (document.getElementById ("saque").checked){
         var saque = parseFloat(document.getElementById("saq2").value);
